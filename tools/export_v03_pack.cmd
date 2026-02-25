f03fc06e60d4f0179a58efd98413c246df60bf1d
Jose Ramos <vantagebizmedia@gmail.com>
2026-02-25 16:39:58 -0500

    Hotfix: export pack defaults to config workspace exports

--- tools/export_v03_pack.cmd
+++ tools/export_v03_pack.cmd
@@ -1,7 +1,8 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
+
 REM Uso:
-REM   tools\export_v03_pack.cmd "<manifest_path>" [--overwrite]
+REM   tools\export_v03_pack.cmd "<manifest_path>" [--overwrite] [--out-root "..."]
 REM Ej:
 REM   tools\export_v03_pack.cmd "_v03_smoke_cfg\artifacts\manifest_v03.json" --overwrite
 
@@ -12,6 +13,15 @@ if "%MAN%"=="" (
   exit /b 1
 )
 
+REM Construye RESTO args desde %2..%9 sin romper comillas
+set "REST="
 shift
-python tools\export_v03_pack.py --manifest "%MAN%" %*
+:LOOP
+if "%~1"=="" goto RUN
+set "REST=%REST% %~1"
+shift
+goto LOOP
+
+:RUN
+python tools\export_v03_pack.py --manifest "%MAN%" %REST%
 endlocal
